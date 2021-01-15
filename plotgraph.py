e880bbc75438158bdd8494e61b850e99d977d5e4
nsaljoughi <nicolasaljoughi@gmail.com>
2021-01-15 16:12:13 +0100

    update

--- plotgraph.py
+++ plotgraph.py
@@ -10,8 +10,13 @@ def get_num(line, startidx, sep1, sep2):
 current_dir = os.path.dirname(os.path.realpath(__file__))
 build_dir = os.path.abspath(os.path.join(current_dir, 'build'))
 
+DIFF_FILT = []
+DIFF_UNFILT = []
+ROT_FILT = []
+ROT_UNFILT = []
+
 for filename in os.listdir(build_dir):
-    if (filename == "results_filt.txt") or (filename == "results_unfilt.txt"):
+    if (filename == "results_filt.txt"):
         flog = os.path.abspath(os.path.join(build_dir, filename))
         with open(flog) as f:
             lines = f.readlines()
@@ -32,25 +37,62 @@ for filename in os.listdir(build_dir):
             FRAME.append(int(frame))
             DIFF.append(float(diff))
             DIFF_ROT.append(float(diff_rot))
-            
-        fig = plt.figure('Results for {}'.format(filename), figsize=(9, 14))
-        
-        plt.subplot(2, 1, 1)
-        plt.plot(DIFF, color='blue', linewidth=2)
-        plt.title('Difference in translation', fontsize=10)
-        plt.xlabel('frames', fontsize=10)
-        plt.ylabel('||x(t) - x(t-1)||', fontsize=10)
-        plt.grid(True)
-        
-        plt.subplot(2, 1, 2)
-        plt.plot(DIFF_ROT, color='green', linewidth=2)
-        plt.title('Difference in rotation', fontsize=10)
-        plt.xlabel('frames', fontsize=10)
-        plt.ylabel('||q(t) - q(t-1)||', fontsize=10)
-        plt.grid(True)
-        
-        fig.tight_layout()
-        plt.show()
 
+        DIFF_FILT = DIFF
+        ROT_FILT = DIFF_ROT
+
+    elif (filename == "results_unfilt.txt"):
+        flog = os.path.abspath(os.path.join(build_dir, filename))
+        with open(flog) as f:
+            lines = f.readlines()
+
+        FRAME = []
+        DIFF = []
+        DIFF_ROT = []
+
+        for line in lines:
+            frameidx = line.find('Frame')
+            diffrotidx = line.find('Rot')
+            diffidx = line.find('Dist')
+
+            frame = get_num(line, frameidx, ' ', ',')
+            diff_rot = get_num(line, diffrotidx, ' ', ',')
+            diff = get_num(line, diffidx, ' ', ';')
+
+            FRAME.append(int(frame))
+            DIFF.append(float(diff))
+            DIFF_ROT.append(float(diff_rot))
+
+        DIFF_UNFILT = DIFF
+        ROT_UNFILT = DIFF_ROT
+        
     else:
         continue
+
+#plt.style.use('dark_background')
+
+fig = plt.figure('Results for {}'.format(filename), figsize=(20, 14))
+
+plt.subplot(2, 1, 1)
+plt.plot(DIFF_FILT, color='red', linewidth=4, label='Filtered')
+plt.plot(DIFF_UNFILT, color='orange', linewidth=2, label='Unfiltered')
+plt.title('Difference in translation', fontsize=10)
+plt.xlabel('frames', fontsize=10)
+plt.ylabel('||x(t) - x(t-1)||', fontsize=10)
+plt.ylim(-0.5, 3)
+plt.grid(True)
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(ROT_FILT, color='blue', linewidth=4, label='Filtered')
+plt.plot(ROT_UNFILT, color='green', linewidth=2, label='Unfiltered')
+plt.title('Difference in rotation', fontsize=10)
+plt.xlabel('frames', fontsize=10)
+plt.ylabel('||q(t) - q(t-1)||', fontsize=10)
+plt.grid(True)
+plt.legend()
+
+fig.tight_layout()
+plt.savefig('results_plot.png', dpi=300)
+plt.show()
+
