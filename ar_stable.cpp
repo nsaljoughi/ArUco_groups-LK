06676aa3a84ab8e523a8a158aaaba0f906a0e014
Nicola Saljoughi <nicolasaljoughi@gmail.com>
2021-03-19 11:21:41 +0100

    Update ar_stable.cpp

--- ar_stable.cpp
+++ ar_stable.cpp
@@ -449,15 +449,16 @@ bool checkPoseConsistent(std::vector<Vec3d> rvecs_ord, std::vector<bool> detect_
             bool fail=false;
 
             for(int k=0; k<3; k++) {
+
                 cout << rvecs[i][k] << endl;
                 cout << rvecs[j][k] << endl;
                 cout << "Angle diff " << std::abs(rvecs[i][k]-rvecs[j][k]) << endl;
                 cout << "Angle diff with sin " << std::abs(sin(rvecs[i][k])-sin(rvecs[j][k])) << endl;
                 cout << (std::abs(sin(rvecs[i][k])-sin(rvecs[j][k])) > sin(thr[k])) << endl;
-                if(std::abs(rvecs[i][k]-rvecs[j][k]) > thr[k]) {
+
+                if(std::abs(sin(rvecs[i][k])-sin(rvecs[j][k])) > sin(thr[k])) {
                     unconsistent += 1;
                     fail = true;
-                    cout << "Fail" << endl;
                     break;
                 }
             }
