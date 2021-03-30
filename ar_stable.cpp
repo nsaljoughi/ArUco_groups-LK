923d960a97d67066e5f277c0000b76930c087f56
Nicola Saljoughi <nicolasaljoughi@gmail.com>
2021-03-30 10:49:13 +0200

    Update ar_stable.cpp

--- ar_stable.cpp
+++ ar_stable.cpp
@@ -977,6 +977,10 @@ int main(int argc, char *argv[]) {
                     circle(imageCopy, arrow4[j], 1, Scalar(0,0,255), -1);
                 }
             }
+            line(imageCopy, box1[0], box1[1], Scalar(0,0,0), 2, LINE_8);
+            line(imageCopy, box1[1], box1[2], Scalar(0,0,0), 2, LINE_8);
+            line(imageCopy, box1[2], box1[3], Scalar(0,0,0), 2, LINE_8);
+            line(imageCopy, box1[3], box1[0], Scalar(0,0,0), 2, LINE_8);
         }
 
         if(showRejected && rejected.size() > 0)
