724129370fd793d93b00db86cff3aca3dc123961
Nicola Saljoughi <nicolasaljoughi@gmail.com>
2021-03-31 14:44:00 +0200

    Update ar_stable.cpp

--- ar_stable.cpp
+++ ar_stable.cpp
@@ -1287,14 +1287,15 @@ int main(int argc, char *argv[]) {
             rScene = computeSceneRot(rMaster, detect_id, init_id, 1);
 
             Vec3d tvec1, tvec2;
-            tvec1[2] = 2.2;
-            tvec1[1] = -1.1;
-            tvec1[0] = 0.0;
+            tvec1[0] = 0.0//-2.2;
+            tvec1[1] = 0.0//-1.1;
+            tvec1[2] = 3.0;
             tvec2[0] = - 1.3;
             tvec2[1] = 1.2;
             tvec2[2] = 0.0;
 
             rMaster[0] = rotateAxis(rMaster[0]);
+
             tvec1 = transformVec(tvec1, rMaster[0], tMaster[0]);
             tvec2 = transformVec(tvec2, rMaster[1], tMaster[1]);
 
