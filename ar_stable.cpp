d9a32def8c0fa69f7a4da7df530966e5187e9ef3
Nicola Saljoughi <nicolasaljoughi@gmail.com>
2021-03-31 14:55:00 +0200

    Update ar_stable.cpp

--- ar_stable.cpp
+++ ar_stable.cpp
@@ -1288,7 +1288,7 @@ int main(int argc, char *argv[]) {
 
             Vec3d tvec1, tvec2;
             tvec1[0] = 2.2;
-            tvec1[1] = 0.0;//1.1;
+            tvec1[1] = 1.1;
             tvec1[2] = 0.0;
             tvec2[0] = - 1.3;
             tvec2[1] = 1.2;
