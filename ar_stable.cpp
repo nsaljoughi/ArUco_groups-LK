375c8868231b8c73d5f5bada3e30977276bf8140
Nicola Saljoughi <nicolasaljoughi@gmail.com>
2021-03-31 11:46:27 +0200

    Update ar_stable.cpp

--- ar_stable.cpp
+++ ar_stable.cpp
@@ -1125,7 +1125,7 @@ int main(int argc, char *argv[]) {
             tvec1[0] = -2.2;
             tvec1[1] = -1.1;
             tvec1[2] = 0.0;
-            tvec2[0] = - 1.2;
+            tvec2[0] = - 1.3;
             tvec2[1] = 1.2;
             tvec2[2] = 0.0;
 
